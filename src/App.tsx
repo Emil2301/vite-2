b501c29702091349e77283cb99a799e8df0aba51
Emil Kawałko <e.kawalko@sportradar.com>
2022-12-21 10:41:13 +0100

    add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useQuery } from 'react-query'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const { isLoading, error, data } = useQuery('repoData', () =>
+        fetch('https://jsonplaceholder.typicode.com/todos/1').then(res =>
+            res.json()
+        )
+    )
+    if (isLoading) return 'Loading...'
+    if (error) return 'An error has occurred: '
   return (
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more, hello
-      </p>
+        <div>
+            <strong>👀 {data.title}</strong>{' '}
+            <strong>✨ {data.id}</strong>{' '}
+            <strong>🍴 {data.forks_count}</strong>
+        </div>
     </div>
   )
 }
